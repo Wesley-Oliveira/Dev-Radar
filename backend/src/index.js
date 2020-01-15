e7e225af0d34d446132755b24e0c91ab4faeace9
Wesley-Oliveira <wesley0leby@gmail.com>
2020-01-15 16:05:23 -0300

    Configuração inicial do express, criação de models e controllers para cadastro e busca de usuários.

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+
+// Importando express, mongoose e routes
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+// Criando conexão com o banco de dados mongo
+mongoose.connect('mongodb+srv://admin:admin123@cluster0-gke9r.mongodb.net/devradar?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Para o express entender que será utilizado JSON
+app.use(express.json());
+// Para poder utilizar as rotas exportadas
+app.use(routes);
+
+// Selecionando porta
+app.listen(3333);
---
+++ backend/src/models/utils/PointSchema.js
@@ -0,0 +1,16 @@
+// Importando mongoose
+const mongoose = require('mongoose');
+
+const PointSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+    },
+    coordinates: {
+        type: [Number],
+        required: true,
+    },
+});
+
+module.exports = PointSchema;
